576cfca579d8cef5fe9e10798b6f4cc1fa6c2112
Edvaldo José <edvaldojose003@gmail.com>
2023-06-21 09:32:21 -0300

    feat: tests ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,23 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    frango = Ingredient('arroz')
+    carne = Ingredient('feijao')
+    camarao = Ingredient('arroz')
+
+    assert hash(frango) == hash('arroz')
+    assert hash(carne) == hash('feijao')
+    assert hash(frango) != hash('feijao')
+
+    assert frango == camarao
+    assert frango != carne
+
+    assert frango.name == 'arroz'
+
+    assert frango.restrictions == set()
+  
+    equal = camarao == frango
+    assert frango.__eq__(camarao) == equal
+
+    assert frango.__repr__() == "Ingredient('arroz')"
+
